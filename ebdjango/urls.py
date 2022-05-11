2228e227d55ae9fd5af7a7dcd17257b5df71ba10
Nuno Silva <de.moi.8.ns@gmail.com>
2022-05-11 11:50:43 +0100

    1
    Arranjar maneira de dar reply do react

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('kitchen/', include('kitchen.urls')),
     path('customers/', include('customers.urls')),
     path('admin/', admin.site.urls),
 ]
