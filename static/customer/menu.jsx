2beffaff9cf2b52042af768d889bc89f26e4e6df
Diogo Jordão Filipe <diogojordao.2504@gmail.com>
2022-05-21 13:53:24 +0100

    Update menu

--- static/customer/menu.jsx
+++ static/customer/menu.jsx
@@ -10,7 +10,7 @@ class MyPage extends React.Component {
       super(props)
       this.state = {
           items: [],
-          selectedCategory: "",
+          selectedCategory: "Appetizer",
           order: [],
           alertMessage: "",
       }
@@ -45,60 +45,101 @@ class MyPage extends React.Component {
 
   addItem(item) {
     let array = this.state.order;
-    array.push(item.name)
+    let name = item.name.S;
+    let price = item.price.N;
+    let newItem = {name, price}
+    array.push(newItem)
     this.setState({order: array});
+    console.log(this.state.order);
+  }
+
+  removeItem(item) {
+    let array = this.state.order;
+
+    let index = array.indexOf(item);
+    if (index !== -1) {
+      array.splice(index, 1);
+    }
+
+    this.setState({order: array});
+    console.log(this.state.order);
   }
 
   render() {
-      return (
-            <div>
-              <h3>Menu</h3>
-              <Reactstrap.ListGroup>
-                <Reactstrap.ListGroupItem tag="button" onClick={() => this.setCategory("Appetizer")} action>Appetizers</Reactstrap.ListGroupItem>
-                <Reactstrap.ListGroupItem tag="button" onClick={() => this.setCategory("Soup")} action>Soups</Reactstrap.ListGroupItem>
-                <Reactstrap.ListGroupItem tag="button" onClick={() => this.setCategory("Main Course")} action>Main Courses</Reactstrap.ListGroupItem>
-                <Reactstrap.ListGroupItem tag="button" onClick={() => this.setCategory("Drink")} action>Drinks</Reactstrap.ListGroupItem>
-                <Reactstrap.ListGroupItem tag="button" onClick={() => this.setCategory("Dessert")} action>Desserts</Reactstrap.ListGroupItem>
-              </Reactstrap.ListGroup>
+    return (
+      <div>
+        <h3>Menu</h3>
+        <Reactstrap.ListGroup>
+          <Reactstrap.ListGroupItem tag="button" onClick={() => this.setCategory("Appetizer")} action>Appetizers</Reactstrap.ListGroupItem>
+          <Reactstrap.ListGroupItem tag="button" onClick={() => this.setCategory("Soup")} action>Soups</Reactstrap.ListGroupItem>
+          <Reactstrap.ListGroupItem tag="button" onClick={() => this.setCategory("Main Course")} action>Main Courses</Reactstrap.ListGroupItem>
+          <Reactstrap.ListGroupItem tag="button" onClick={() => this.setCategory("Drink")} action>Drinks</Reactstrap.ListGroupItem>
+          <Reactstrap.ListGroupItem tag="button" onClick={() => this.setCategory("Dessert")} action>Desserts</Reactstrap.ListGroupItem>
+        </Reactstrap.ListGroup>
               
-              <table>
-                <thead>
-                  <tr>
-                    <th>Item</th>
-                    <th>Description</th>
-                    <th>Calories</th>
-                    <th>€</th>
-                  </tr>
-                </thead>
-
-              <tbody>
-                {!this.state.items || this.state.items.length <= 0 ? (
-                <tr>
-                  <td colSpan="6" align="center">
-                  <b>Oops, no food here yet</b>
-                  </td>
-                </tr>) : 
-                (
-                  this.state.items.map(item => (
-                    item.category.S == this.state.selectedCategory ?
-                      <tr colSpan="6" align="center" key={item.food_id.N}>
-                      <td>{item.name.S}</td>
-                      <td>{item.description.S}</td>
-                      <td>{item.calories.N}</td>
-                      <td>{item.price.N}</td>
-                      <Reactstrap.Button onClick={() => this.addItem(item)}>Add</Reactstrap.Button>
-                      </tr>
-                    
-                    :
-                    null
-                  )
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Description</th>
+              <th>Calories</th>
+              <th>€</th>
+            </tr>
+          </thead>
+          <tbody>
+            {!this.state.items || this.state.items.length <= 0 ? (
+              <tr>
+                <td colSpan="6" align="center">
+                <b>Oops, no food here yet</b>
+                </td>
+              </tr>) : 
+                (this.state.items.map(item => (
+                item.category.S == this.state.selectedCategory ?
+                  <tr colSpan="6" align="center" key={item.food_id.N}>
+                    <td>{item.name.S}</td>
+                    <td>{item.description.S}</td>
+                    <td>{item.calories.N}</td>
+                    <td>{item.price.N}</td>
+                    <Reactstrap.Button onClick={() => this.addItem(item)}>Add</Reactstrap.Button>
+                  </tr> :
+                    null)
                   )
                 )
               }
-              </tbody>
-            </table>
-            </div>
-      );
+            </tbody>
+        </table>
+
+        <h3>Order</h3>
+        <table>
+          <tbody>
+            {!this.state.order || this.state.order.length <= 0 ? (
+              <tr>
+                <td colSpan="6" align="center">
+                <b>You haven't added any food items yet.</b>
+                </td>
+              </tr>) : 
+                (this.state.order.map(item => (
+                  <tr colSpan="6" align="center">
+                    <td>{item.name}</td>
+                    <td>{item.price}</td>
+                    <Reactstrap.Button onClick={() => this.removeItem(item)}>Remove</Reactstrap.Button>
+                  </tr>
+                  )
+                )
+              )
+            }
+          </tbody>
+        </table>
+
+        <form>
+          <label>
+            Location tag:
+            <input type="number" name="locationTag" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
 
