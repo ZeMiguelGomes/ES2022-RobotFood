5ba727b4fcc49631781c463aa166d882adf941b5
Jose Miguel Silva Gomes <jose.miguel.gomes@hotmail.com>
2022-05-25 10:09:15 +0100

    Minor changes

--- customer/views.py
+++ customer/views.py
@@ -9,14 +9,15 @@ from django.http import JsonResponse
 get_food_items = "arn:aws:states:us-east-1:936322414606:stateMachine:GetFoodItems"
 calc_price = "arn:aws:states:us-east-1:936322414606:stateMachine:CalcPrice"
 get_food_itemsZe = "arn:aws:states:us-east-1:067458896719:stateMachine:GetFoodItems"
+calc_priceZe = "arn:aws:states:us-east-1:067458896719:stateMachine:calc_price"
 
 @api_view(['GET'])
 def getFoodItems(request):
     if request.method == 'GET':
-        #sf = boto3.client('stepfunctions', region_name='us-east-1')
-        sf = boto3.client('stepfunctions', region_name='us-east-1', aws_access_key_id="ASIA5UAJKHAHMCM3DHHO", aws_secret_access_key="HJwINA/SrPTHTnFauAeeEwChPfnJRnLkXHt6J1uT", aws_session_token="FwoGZXIvYXdzEJf//////////wEaDEdymnZm/Yz9l2MXfiLLAegXb7svd0Z9NeTFT1ra6NPI0JlFmjOFGksAie4bTRjsHVdMOMw2bog3kzplOKvKZ/z3h57T6561wDGcibx/GSIDuc4qHbmfG+0BEzoJdw376tXjVHa4gxQPmHvAAobAr3a3QJEbu4+vs08x9AIPjZdk6eOGvirc317k/tCQu+csKtD/l20kjAXrco+m9bRua0MnC1aCLX3Z2n8xJITleGOrWCJxzJUFNSVmFKkAmc8lWeJmdGGxe9TMeevp4+aTNn6kRgkcj6lHxStlKIbPsZQGMi0sFPGSK4MAJsYoTySXZ9AOC4OwE/yPZNctRBKGsiXmBgGPsKIvoYJXXT83DMw=")
+        sf = boto3.client('stepfunctions', region_name='us-east-1')
+        #sf = boto3.client('stepfunctions', region_name='us-east-1', aws_access_key_id="ASIAQ7NG5CNH73SBWST6", aws_secret_access_key="aoRQCnDTIJZGwYeUO0BGthp2859KZ6ZX7zh4n5OU", aws_session_token="FwoGZXIvYXdzEJr//////////wEaDEuh2WBu4jdO5m9WQSLLAfXsgJHyQaAjY3fJmLDeWtlWfu9e6Mzi4RoXSPn6XfyB/QcLlqFl2J1yDSZE1SrwSy3R/6UHQ/4U1huGoSNN3ec364+bB5ujcILkJk/HsZTrwLZNSp9f5n3Y5P7HowHiQkgKgzF65jfzy4cBU0T5y2sjsVGjHJ1+nTafyMk0RfUxJjfy5GEXDrsFmXeBn0mdsJfukO94dBxeCcH2XQuSeQIKdO0JVy80q89JR+DQ7m6wcDbjcAq/GEwQ1QVTfLqafb8NPNQqubmFhie1KLu3spQGMi3imZFy63Ie+A6JvVydiQ6XxylJnb7YTxb1N9K/cHjQIinx+U5XQCEFe4WA8UU=")
         
-        res = sf.start_sync_execution(stateMachineArn=get_food_items)
+        res = sf.start_sync_execution(stateMachineArn=get_food_itemsZe)
         data = json.loads(res["output"])
         
         return JsonResponse(data, safe=False)
@@ -41,20 +42,21 @@ def uploadPhoto(request):
         data = request.data.dict()
         print(data)
         #s3 = boto3.client('s3', region_name='us-east-1')
-        s3 = boto3.client('s3', region_name='us-east-1', aws_access_key_id="ASIA5UAJKHAHMCM3DHHO", aws_secret_access_key="HJwINA/SrPTHTnFauAeeEwChPfnJRnLkXHt6J1uT", aws_session_token="FwoGZXIvYXdzEJf//////////wEaDEdymnZm/Yz9l2MXfiLLAegXb7svd0Z9NeTFT1ra6NPI0JlFmjOFGksAie4bTRjsHVdMOMw2bog3kzplOKvKZ/z3h57T6561wDGcibx/GSIDuc4qHbmfG+0BEzoJdw376tXjVHa4gxQPmHvAAobAr3a3QJEbu4+vs08x9AIPjZdk6eOGvirc317k/tCQu+csKtD/l20kjAXrco+m9bRua0MnC1aCLX3Z2n8xJITleGOrWCJxzJUFNSVmFKkAmc8lWeJmdGGxe9TMeevp4+aTNn6kRgkcj6lHxStlKIbPsZQGMi0sFPGSK4MAJsYoTySXZ9AOC4OwE/yPZNctRBKGsiXmBgGPsKIvoYJXXT83DMw=")
-        s3.put_object(Body=data['file'], Bucket='face-to-detect', Key=data['file'].name)
+        s3 = boto3.client('s3', region_name='us-east-1', aws_access_key_id="ASIAQ7NG5CNHSVKAVRNZ", aws_secret_access_key="k1YoeJjhoN4xkQLe5A6ngXowkL+pFMDD2jMuMkrM", aws_session_token="FwoGZXIvYXdzEKP//////////wEaDO6wrjs8Gu4R+xKVQSLLAVvgUyQkzcIm6WSHZ16AjNJXKTbtTxdQfaH8oa+56Vz4TwDAjTaCoQDl7TZDpz/Pd3XAjDVpvllf39De8fXJLo77mp1RMwjgfO0siXcMHTKshQ+sA+/8IVE73nML5+RGUMysTSkXdHWSLaEOobVPBEWuMz3I9jXGndmTH+BDqxNVfte4sJx9brj6WWDYwvRMbbyu3GuPnku1oRZ9MLs6fBwcoZakO/goWCeEOncMDsLQHGCyuG3kmVN4UNJBEmZd+N8/sU5PgwUoSAKGKLuttJQGMi384c8WHp6EVkQp2lIqnaehENv9KRrC949D1oqHgQlj0b63R9IO57POtWskTnY=")
+        s3.put_object(Body=data['file'], Bucket='facetodetect-es', Key=data['file'].name)
         #object = s3.Object('face-to-detect', data['file'].name)
         #ret = object.put(Body=data['file'])
+
 
         return JsonResponse(True, safe=False)
 
 @api_view(['POST'])  
 def orderPrice(request):
     if request.method == 'POST':
-        #sf = boto3.client('stepfunctions', region_name='us-east-1')
-        sf = boto3.client('stepfunctions', region_name='us-east-1', aws_access_key_id="ASIA5UAJKHAHMCM3DHHO", aws_secret_access_key="HJwINA/SrPTHTnFauAeeEwChPfnJRnLkXHt6J1uT", aws_session_token="FwoGZXIvYXdzEJf//////////wEaDEdymnZm/Yz9l2MXfiLLAegXb7svd0Z9NeTFT1ra6NPI0JlFmjOFGksAie4bTRjsHVdMOMw2bog3kzplOKvKZ/z3h57T6561wDGcibx/GSIDuc4qHbmfG+0BEzoJdw376tXjVHa4gxQPmHvAAobAr3a3QJEbu4+vs08x9AIPjZdk6eOGvirc317k/tCQu+csKtD/l20kjAXrco+m9bRua0MnC1aCLX3Z2n8xJITleGOrWCJxzJUFNSVmFKkAmc8lWeJmdGGxe9TMeevp4+aTNn6kRgkcj6lHxStlKIbPsZQGMi0sFPGSK4MAJsYoTySXZ9AOC4OwE/yPZNctRBKGsiXmBgGPsKIvoYJXXT83DMw=")
+        sf = boto3.client('stepfunctions', region_name='us-east-1')
+        #sf = boto3.client('stepfunctions', region_name='us-east-1', aws_access_key_id="ASIAQ7NG5CNH73SBWST6", aws_secret_access_key="aoRQCnDTIJZGwYeUO0BGthp2859KZ6ZX7zh4n5OU", aws_session_token="FwoGZXIvYXdzEJr//////////wEaDEuh2WBu4jdO5m9WQSLLAfXsgJHyQaAjY3fJmLDeWtlWfu9e6Mzi4RoXSPn6XfyB/QcLlqFl2J1yDSZE1SrwSy3R/6UHQ/4U1huGoSNN3ec364+bB5ujcILkJk/HsZTrwLZNSp9f5n3Y5P7HowHiQkgKgzF65jfzy4cBU0T5y2sjsVGjHJ1+nTafyMk0RfUxJjfy5GEXDrsFmXeBn0mdsJfukO94dBxeCcH2XQuSeQIKdO0JVy80q89JR+DQ7m6wcDbjcAq/GEwQ1QVTfLqafb8NPNQqubmFhie1KLu3spQGMi3imZFy63Ie+A6JvVydiQ6XxylJnb7YTxb1N9K/cHjQIinx+U5XQCEFe4WA8UU=")
         
-        res = sf.start_sync_execution(stateMachineArn=calc_price, input=json.dumps(request.data['order']))
+        res = sf.start_sync_execution(stateMachineArn=calc_priceZe, input=json.dumps(request.data['order']))
         data = json.loads(res["output"])
         print(data)
 
